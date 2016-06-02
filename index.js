87651d24052d6a747f6c1b1ac79be996519d3942
Tom Dowding <tom@mobilepie.com>
2016-06-02 17:40:06 +0200

    trying index fixes

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
-  emailAdapter: simpleMailgunAdapter
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
