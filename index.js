ac2e83749334b7de2db3fdf018b499851e8f7e13
Tom Dowding <tom@mobilepie.com>
2016-06-07 14:36:54 +0200

    no message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
-var SimpleMailgunAdapter = require('parse-server/lib/Adapters/Email/SimpleMailgunAdapter');
+//var SimpleMailgunAdapter = require('parse-server/lib/Adapters/Email/SimpleMailgunAdapter');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
@@ -22,7 +22,15 @@ var api = new ParseServer({
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
   publicServerURL: process.env.PUBLIC_SERVER_URL || 'http://localhost:1337/parse',
-  appName: process.env.APP_NAME || 'Mayfly Sounds'
+  appName: process.env.APP_NAME || 'Mayfly Sounds',
+  emailAdapter: {
+    module: 'parse-server-simple-mailgun-adapter',
+    options: {
+      fromAddress: 'EMAIL@DOMAIN',
+      domain: 'app51c87c84b4c34b27a02cf8aa0581ba54.mailgun.org',
+      apiKey: 'key-0e17afb6706237c8dcfa11434698383d'
+    }
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
